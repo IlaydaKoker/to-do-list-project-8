848d549a3f250d5964d8fa914a418526c7736c0e
Ilayda Koker <ilayda.n.koker@gmail.com>
2023-07-18 10:21:23 -0700

    Initial Commit

---
+++ to-do list project 8/to_do_list_project_8App.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_list_project_8App.swift
+//  to-do list project 8
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct to_do_list_project_8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
